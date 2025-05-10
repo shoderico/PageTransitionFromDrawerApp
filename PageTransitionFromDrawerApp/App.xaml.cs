8337fd0b3d140e048cf79e0112caf4680c7d023b
shoderico <yanagida@shoderitz.com>
2025-05-11 08:03:18 +0900

    initial commit: Visual Studio MAUI App Wizard

---
+++ PageTransitionFromDrawerApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PageTransitionFromDrawerApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PageTransitionFromDrawerApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PageTransitionFromDrawerApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
